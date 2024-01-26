fb53c942fbaaa2cdd8969c5ffa6e3d830b24d569
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2024-01-26 01:45:03 +0100

    Adjusted some imports. MovementGenerator class has been created.

--- src/main/java/it/unibo/jetpackjoyride/core/hitbox/Hitbox.java
+++ src/main/java/it/unibo/jetpackjoyride/core/hitbox/Hitbox.java
@@ -8,18 +8,18 @@ import it.unibo.jetpackjoyride.utilities.Pair;
 /* An interface for the hitbox of entities */
 
 public interface Hitbox {
-    /* The general idea is to model the hitbox of an entity with a rectangle. More precision in hitbox collision detection 
-    could be achieved by using more rectangles instead of only one, but problems may arise when having to deal woth
-    rotating objects so I'll just stick to the one-rectangle hitbox solution.
-     */
+  /* The general idea is to model the hitbox of an entity with a rectangle. More precision in hitbox collision detection 
+  could be achieved by using more rectangles instead of only one, but problems may arise when having to deal woth
+  rotating objects so I'll just stick to the one-rectangle hitbox solution.
+   */
 
-       void setHitboxOn();
+     void setHitboxOn();
 
-       void setHitboxOff();
+     void setHitboxOff();
 
-       boolean isHitboxOn();
-       
-       void updateHitbox(Pair<Double, Double> newPosition);
+     boolean isHitboxOn();
+     
+     void updateHitbox(Pair<Double, Double> newPosition);
 
-       Pair<Double,Double> getHitboxPosition();
+     Pair<Double,Double> getHitboxPosition();
 }

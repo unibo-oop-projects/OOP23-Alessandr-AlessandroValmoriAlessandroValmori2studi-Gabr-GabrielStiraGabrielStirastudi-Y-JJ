ab019ce22c7b32ad3bc5bd54b237e7cf56eab2cf
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2024-01-26 23:36:07 +0100

    Completed MovementGenerator class: now can generate all Movements for most entities

--- src/main/java/it/unibo/jetpackjoyride/core/movement/AbstractMovement.java
+++ src/main/java/it/unibo/jetpackjoyride/core/movement/AbstractMovement.java
@@ -1,5 +1,9 @@
 package it.unibo.jetpackjoyride.core.movement;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unibo.jetpackjoyride.core.movement.MovementGenerator.MovementChangers;
 import it.unibo.jetpackjoyride.utilities.Pair;
 
 public abstract class AbstractMovement implements Movement {
@@ -8,14 +12,18 @@ public abstract class AbstractMovement implements Movement {
     public Pair<Double, Double> currentPosition;
     public Pair<Double,Double> xyacceleration;
     public Pair<Double,Double> xyspeed;
-    public Double time = 0.1;
-    public Double speedModifier = 1.0;
 
-    public AbstractMovement(Pair<Double, Double> startingPosition, Pair<Double, Double> startingSpeed, Pair<Double, Double> startingAcceleration) {
+    private List<MovementChangers> listOfChangers = new ArrayList<>();
+
+    public final static Pair<Double,Double> playerPos = new Pair<>(0.0, 10.0);
+    public final static Double TIME = 0.1;
+
+    public AbstractMovement(Pair<Double, Double> startingPosition, Pair<Double, Double> startingSpeed, Pair<Double, Double> startingAcceleration, List<MovementChangers> listOfChangers) {
         this.startingPosition = startingPosition;
         this.currentPosition = startingPosition;
         this.xyspeed = startingSpeed;
         this.xyacceleration = startingAcceleration;
+        this.listOfChangers = listOfChangers;
     }
 
     public Pair<Double, Double> getStartingPosition() {
@@ -51,6 +59,16 @@ public abstract class AbstractMovement implements Movement {
     public Pair<Double, Double> getSpeed() {
         return this.xyspeed;
     }
+
+    public List<MovementChangers> getMovementChangers() {
+        return this.listOfChangers;
+    }
+
+    public void setMovementChangers( List<MovementChangers> listOfChangers) {
+        this.listOfChangers = listOfChangers;
+    }
+
+    public abstract void applyModifiers();
 
     public abstract void update();
 }

74ee12a86fa88fc2b6beb7a26f354c03936945c5
Yukai Zhou <yuaki.zhou@studio.unibo.it>
2024-01-26 01:17:50 +0100

    junit-platform-console-standalone-1.9.1 add in dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     // JUnit API and testing engine
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+	testImplementation("org.junit.platform:junit-platform-console-standalone:1.9.1")
 }
 
 tasks.withType<Test> {

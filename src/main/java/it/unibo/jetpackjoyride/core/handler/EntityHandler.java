55adb5cf0292fc6deaff17f1bf5554e6a5c3bdde
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2024-01-27 17:06:31 +0100

    Created a temporary EntityView to check if the obstacle movement implementation works

--- src/main/java/it/unibo/jetpackjoyride/core/handler/EntityHandler.java
+++ src/main/java/it/unibo/jetpackjoyride/core/handler/EntityHandler.java
@@ -1,5 +1,12 @@
 package it.unibo.jetpackjoyride.core.handler;
 
+import javafx.scene.image.ImageView;
+
 public interface EntityHandler {
     void update();
+
+    void initialize();
+
+    ImageView getImageView();
+
 }
---
+++ src/main/java/it/unibo/jetpackjoyride/core/handler/EntityView.java
@@ -0,0 +1,32 @@
+package it.unibo.jetpackjoyride.core.handler;
+
+import it.unibo.jetpackjoyride.core.entities.entity.api.Entity;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+
+public class EntityView {
+    private ImageView imageView;
+    private Image[] images;
+    private int animationFrame;
+
+    public EntityView(Image[] images) {
+        this.images = images;
+        imageView = new ImageView();
+        this.animationFrame = 0;
+    }
+
+    public void updateView(Entity entity) {
+        imageView.setX(entity.getEntityMovement().getCurrentPosition().get1());
+        imageView.setY(entity.getEntityMovement().getCurrentPosition().get2());
+        imageView.setFitWidth(200);
+        imageView.setFitHeight(50);
+
+        imageView.setImage(images[animationFrame]);
+        animationFrame = (animationFrame + 1) % images.length;
+
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+}

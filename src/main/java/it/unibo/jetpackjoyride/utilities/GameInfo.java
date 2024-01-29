236c8db57ad53d0f200688edbeff43ea5f32b018
Yukai Zhou <yukai.zhou@studio.unibo.it>
2024-01-29 18:44:16 +0100

    add global static variable move Speed in GameInfo

--- src/main/java/it/unibo/jetpackjoyride/utilities/GameInfo.java
+++ src/main/java/it/unibo/jetpackjoyride/utilities/GameInfo.java
@@ -2,17 +2,18 @@ package it.unibo.jetpackjoyride.utilities;
 
 
 public class GameInfo {
+    public static int moveSpeed = 2;
+
     private double screenWidth;
     private double screenHeight;
     private double screenRatio;
+    
 
     public GameInfo(double screenWidth, double screenHeight) {
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
         this.screenRatio = this.screenWidth/this.screenHeight;
     }
-
-    
 
     public void updateInfo(double screenWidth, double screenHeight){
         this.screenWidth = screenWidth;
@@ -30,5 +31,9 @@ public class GameInfo {
 
     public double getScreenRatio() {
         return screenRatio;
+    }
+
+    public void setMoveSpeed(int newSpeed){
+        GameInfo.moveSpeed = newSpeed;
     }
 }

3f8d254a1c633a32219cdc7791547395be359b41
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2024-02-02 15:40:21 +0100

    Fixed actual zapper hitbox size relative to its sprite visualization

--- src/main/java/it/unibo/jetpackjoyride/core/hitbox/impl/ZapperHitbox.java
+++ src/main/java/it/unibo/jetpackjoyride/core/hitbox/impl/ZapperHitbox.java
@@ -5,7 +5,7 @@ import it.unibo.jetpackjoyride.utilities.Pair;
 
 public class ZapperHitbox extends AbstractHitbox  {
     /* Zapper standard dimension (maybe in the future methods to grow or reduce the size could be implemented) */
-    private final static Pair<Double,Double> ZAPPERDIMENSIONS = new Pair<>(200.0,100.0);
+    private final static Pair<Double,Double> ZAPPERDIMENSIONS = new Pair<>(200.0,50.0);
 
     public ZapperHitbox(Pair<Double,Double> hitboxStartingPos, Double startingAngle) {
         super(hitboxStartingPos, ZAPPERDIMENSIONS, startingAngle);

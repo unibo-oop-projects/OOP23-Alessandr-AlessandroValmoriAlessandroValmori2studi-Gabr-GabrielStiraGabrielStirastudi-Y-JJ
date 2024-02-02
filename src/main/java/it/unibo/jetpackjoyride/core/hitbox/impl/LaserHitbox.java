09c2177afaf02a0d95d69b3fdcee6763534eb43a
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2024-02-02 15:45:24 +0100

    Fixed actual laser sprite dimension relative to its hitbox dimension

--- src/main/java/it/unibo/jetpackjoyride/core/hitbox/impl/LaserHitbox.java
+++ src/main/java/it/unibo/jetpackjoyride/core/hitbox/impl/LaserHitbox.java
@@ -7,7 +7,7 @@ import it.unibo.jetpackjoyride.utilities.Pair;
 
 public class LaserHitbox extends AbstractHitbox {
     /* Laser standard dimension (maybe in the future methods to grow or reduce the size could be implemented) */
-    private final static Pair<Double,Double> LASERDIMENSIONS = new Pair<>(1250.0,32.0);
+    private final static Pair<Double,Double> LASERDIMENSIONS = new Pair<>(1150.0,32.0);
 
     public LaserHitbox(Pair<Double,Double> hitboxStartingPos, Double startingAngle) {
         super(hitboxStartingPos, LASERDIMENSIONS, startingAngle);

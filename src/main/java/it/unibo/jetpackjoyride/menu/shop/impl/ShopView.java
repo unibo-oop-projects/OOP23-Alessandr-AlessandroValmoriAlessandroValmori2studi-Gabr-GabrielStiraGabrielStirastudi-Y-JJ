fe43b8d6d58303c2875c2193b474bb6fb9d8a726
Alessandro Valmori <alevalmorii@gmail.com>
2024-02-06 21:18:06 +0100

    try now

--- src/main/java/it/unibo/jetpackjoyride/menu/shop/impl/ShopView.java
+++ src/main/java/it/unibo/jetpackjoyride/menu/shop/impl/ShopView.java
@@ -1,5 +1,34 @@
 package it.unibo.jetpackjoyride.menu.shop.impl;
 
+
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
 public class ShopView {
-    
+    private Scene shopScene;
+    private Pane root;
+
+    public ShopView() {
+        initializeShop();
+    }
+
+    public Scene getScene() {
+        return shopScene;
+    }
+
+    private void initializeShop() {
+        // Create your shop view components
+        root = new Pane();
+        root.setPrefSize(400, 300);
+        
+        Text shopText = new Text("This is the shop view");
+        shopText.setTranslateX(50);
+        shopText.setTranslateY(50);
+        
+        root.getChildren().add(shopText);
+        
+        shopScene = new Scene(root);
+    }
 }

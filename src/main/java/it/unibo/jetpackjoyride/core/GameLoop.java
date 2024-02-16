1cd9dbf555bcd7b98b2eac6c2e050f8e93b0d432
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2024-02-16 17:47:06 +0100

    Don't remove ever again the comment on stopLoop on GameLoop please

--- src/main/java/it/unibo/jetpackjoyride/core/GameLoop.java
+++ src/main/java/it/unibo/jetpackjoyride/core/GameLoop.java
@@ -103,7 +103,7 @@ public final class GameLoop {
                 }
 
                 if(now - lastStatsupdate > statsUpdateInterval){
-                    stopLoop();
+                    //stopLoop();
                     gameStatsHandler.updateModel();
                     lastStatsupdate = now;
                 }

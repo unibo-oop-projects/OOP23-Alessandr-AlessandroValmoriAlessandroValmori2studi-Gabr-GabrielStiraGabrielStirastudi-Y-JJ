76c86c3c647791b7e8d5c363c71b4798e9e5b9c1
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2024-02-18 22:52:00 +0100

    Now images are loaded only one time so it doesn't lag when spawning an entity. All hitboxes now rotate properly.

--- src/main/java/it/unibo/jetpackjoyride/core/handler/obstacle/ObstacleSpawner.java
+++ src/main/java/it/unibo/jetpackjoyride/core/handler/obstacle/ObstacleSpawner.java
@@ -5,8 +5,10 @@ import it.unibo.jetpackjoyride.core.entities.obstacle.api.Obstacle;
 import it.unibo.jetpackjoyride.core.handler.generic.GenericController;
 import it.unibo.jetpackjoyride.utilities.GameInfo;
 
+
 public final class ObstacleSpawner {
     private final ObstacleLoader obstacleLoader;
+
 
     public ObstacleSpawner() {
         this.obstacleLoader = new ObstacleLoader();
@@ -18,41 +20,34 @@ public final class ObstacleSpawner {
             this.obstacleLoader.generatePattern(difficulty);
         }
         return this.obstacleLoader.getInstanceOfPattern();
-
+        //return randomChunk();
     }
-
-    /*private List<GenericController<Obstacle,ObstacleView>> randomChunk() {
+/* 
+    private List<GenericController<Obstacle,ObstacleView>> randomChunk() {
         final Double screenSizeX = GameInfo.getInstance().getScreenWidth();
         final Double screenSizeY = GameInfo.getInstance().getScreenHeight();
         final Double gameMovingSpeed = Double.valueOf(GameInfo.moveSpeed.get());
 
         List<GenericController<Obstacle,ObstacleView>> obstacleControllers = new ArrayList<>();
         Random random = new Random();
-        int numberOfObstacles = random.nextInt(4) + 1;
+        int numberOfObstacles = 10;
 
         for (int i = 0; i < numberOfObstacles; i++) {
-            int typeOfObstacle = random.nextInt(3);
+            int typeOfObstacle = 0;
             int typeOfMovement = random.nextInt(4);
-
             Movement movement;
-            Hitbox hitbox;
             ObstacleType obstacleType;
-            Image[] actualImages;
 
             switch (typeOfObstacle) {
                 case 0:
                     // MISSILE
-
                     movement = new MovementImpl(new Pair<>(screenSizeX - screenSizeX/20, screenSizeY/6 + random.nextDouble(screenSizeY-screenSizeY/6)),
                             new Pair<>(0.0, 0.0), new Pair<>(0.0, 0.0), new Pair<>(0.0, 0.0), List.of(
                                     typeOfMovement == 0 ? MovementChangers.GRAVITY
                                             : typeOfMovement == 1 ? MovementChangers.SLOW
-                                                    : typeOfMovement == 2 ? MovementChangers.HOMING
-                                                            : MovementChangers.SPEEDY,
+                                                    : typeOfMovement == 2 ?MovementChangers.SPEEDY :
                                     MovementChangers.BOUNCING, MovementChangers.SPEEDY));
-                    hitbox = new HitboxImpl(movement.getCurrentPosition(), new Pair<>(150.0, 50.0));
                     obstacleType = ObstacleType.MISSILE;
-                    actualImages = loadImages(0, 19);
                     break;
                 case 1:
                     // ZAPPER
@@ -61,31 +56,22 @@ public final class ObstacleSpawner {
                             new Pair<>(random.nextDouble() * 180,
                                     random.nextInt(2) == 0 ? 0.0 : random.nextDouble() * 5),
                             List.of());
-                    hitbox = new HitboxImpl(movement.getCurrentPosition(), new Pair<>(200.0, 50.0));
                     obstacleType = ObstacleType.ZAPPER;
-                    actualImages = loadImages(20, 39);
                     break;
                 case 2:
                     // LASER
                     movement = new MovementImpl(new Pair<>(screenSizeX / 2, random.nextDouble() * screenSizeY),
                             new Pair<>(0.0, 0.0), new Pair<>(0.0, 0.0), new Pair<>(0.0, 0.0),
                             List.of(MovementChangers.STATIC));
-                    hitbox = new HitboxImpl(movement.getCurrentPosition(), new Pair<>(1150.0, 32.0));
                     obstacleType = ObstacleType.LASER;
-                    actualImages = loadImages(40, 55);
                     break;
                 default:
                     throw new IllegalStateException();
             }
-
-            //Obstacle model = this.entityGenerator.generateObstacle(obstacleType, movement, hitbox);
-
-            ObstacleView view = new ObstacleView(actualImages);
-
-            GenericController<Obstacle,ObstacleView> obstacle = new GenericController<Obstacle,ObstacleView>(model, view);
-            obstacleControllers.add(obstacle);
+            obstacleControllers.add(this.entityGenerator.generateObstacle(obstacleType, movement));
         }
         return obstacleControllers;
-    }*/
+    }
 
+    */
 }

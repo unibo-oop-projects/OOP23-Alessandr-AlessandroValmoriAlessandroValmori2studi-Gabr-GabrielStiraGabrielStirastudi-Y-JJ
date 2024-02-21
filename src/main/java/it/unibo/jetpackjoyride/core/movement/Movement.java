75cea4da2edb9f6a9ab5d1b571b14ea56e092d75
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2024-02-21 04:38:05 +0100

    Added javadoc for Movement class

--- src/main/java/it/unibo/jetpackjoyride/core/movement/Movement.java
+++ src/main/java/it/unibo/jetpackjoyride/core/movement/Movement.java
@@ -5,19 +5,72 @@ import it.unibo.jetpackjoyride.utilities.MovementChangers;
 import it.unibo.jetpackjoyride.utilities.Pair;
 import java.util.*;
 
+/**
+ * The {@link Movement} class is one of the two elements which characterize every entity along with 
+ * the hitbox. 
+ * This class encapsulates elements such as position, speed, acceleration and rotation which allow 
+ * entity to be represented in a two dimensional space. A record is used as a simple data-carrying 
+ * class to store the most important values which characterize the movement.
+ * A list of modifiers is also used to define common behaviours for the classes which use Movement.
+ * This class also provides a method to get a relative position which is the position of the entity 
+ * scaled based on the current screen size.
+ * This is an immutable class, hence the creation of a new Movement class is required every time the
+ * movement has to be updated. While it could seem more resource consuming compared to a mutable
+ * implementation, there are several reason a mutable one can be preferred.
+ * 
+ * @author gabriel.stira@studio.unibo.it
+ */
+
 public final class Movement {
+    /**
+     * Defines how much the speed will be increased downwards.
+     */
     private static final Double GRAVITYMODIFIER = 0.3;
+    /**
+     * Defines how much the speed will be increased upwards.
+     */
     private static final Double INVERSEGRAVITYMODIFIER = -0.3;
+    /**
+     * Defines what coordinate could be considered as the upper bound of the map.
+     */
     private static final Double MAPBOUNDUP = 80.0;
+    /**
+     * Defines what coordinate could be considerated as the lower bound of the map.
+     */
     private static final Double MAPBOUNDDOWN = 630.0;
 
+    /*
+     * A record is used to store the four most important values of the movement class. Since Movement is immutable, 
+     * a record is a good idea to avoid having to implement getters, is more readable and guarantees immutability.
+     */
     record MovCharacterizing(Pair<Double,Double> pos, Pair<Double,Double> speed, Pair<Double,Double> acc, Pair<Double,Double> rot) {}
 
+    /**
+     * Defines the record which will be used to store the movement characteristics.
+     */
     private final MovCharacterizing movementSpecifiers;
+
+    /**
+     * Defines all the modifiers which affect the movement behavior.
+     */
     private final List<MovementChangers> listOfChangers;
 
+    /**
+     * Defines the position of the entity scaled depending on the screen size.
+     */
     private Pair<Double, Double> relativePosition;
 
+     /**
+     * Constructor used to create the instance of the class Movement.
+     *
+     * @param position          The position of the entity (x and y coordinates).
+     * @param speed             The speed of the entity (x and y coordinates).
+     * @param acceleration      The acceleration of the entity (x and y coordinates).
+     * @param rotation          The rotation values of the entity (x and y coordinates).
+     * (v1 is the angle by which the entity is rotated, v2 is how much it rotates with every call of the update method)
+     * @param listOfChangers    The modifiers affecting the entity.
+     * @param relativePosition  The relative position of the entity.
+     */
     private Movement(final Pair<Double, Double> position, final Pair<Double, Double> speed, 
                      final Pair<Double, Double> acceleration, final Pair<Double, Double> rotation, 
                      final  List<MovementChangers> listOfChangers, final Pair<Double, Double> relativePosition) {
@@ -26,30 +79,69 @@ public final class Movement {
         this.relativePosition = relativePosition;
     }
 
+    /**
+     * Gets the relative position of the entity.
+     *
+     * @return The relative position of the entity.
+     */
     public Pair<Double, Double> getRelativePosition() {
         return this.relativePosition;
     }
 
+    /**
+     * Gets the real position of the entity.
+     *
+     * @return The real position of the entity.
+     */
     public Pair<Double, Double> getRealPosition() {
         return this.movementSpecifiers.pos();
     }
 
+    /**
+     * Gets the speed of the entity.
+     *
+     * @return The speedof the entity.
+     */
     public Pair<Double, Double> getSpeed() {
         return this.movementSpecifiers.speed();
     }
 
+    /**
+     * Gets the accelerationof the entity.
+     *
+     * @return The acceleration of the entity.
+     */
     public Pair<Double, Double> getAcceleration() {
         return this.movementSpecifiers.acc();
     }
 
+    /**
+     * Gets the rotation values of the entity.
+     *
+     * @return The rotation values of the entity.
+     */
     public Pair<Double, Double> getRotation() {
         return this.movementSpecifiers.rot();
     }
 
+    /**
+     * Gets all the modifiers of the movement of the entity.
+     *
+     * @return The movement modifiers of the entity.
+     */
     public List<MovementChangers> getMovementChangers() {
         return this.listOfChangers;
     }
 
+    /**
+     * Adapts the given modified position to the screen size.
+     * This method scales the position based on the current screen resolution.
+     * If this wasn't done and the real position would be used, the entity would not 
+     * move as if the new screen sizes were their new bounds to delimit the edges of 
+     * what the player considers to be the map.
+     *
+     * @param modifiedPosition The modified position of the object.
+     */
     private void adaptToScreenSize(final Pair<Double,Double> modifiedPosition) {
         final GameInfo infoResolution = GameInfo.getInstance();
         final Double xScaling = infoResolution.getScreenWidth()/infoResolution.getDefaultWidth();
@@ -58,6 +150,17 @@ public final class Movement {
         this.relativePosition = new Pair<>(modifiedPosition.get1()*xScaling, modifiedPosition.get2()*yScaling);
     }
 
+    /**
+     * Applies the modifiers to the movement depending on what the listOfChangers contains.
+     * Since the possible modifiers are few and a correct order has to be followed when 
+     * applying them, to avoid complications, some if statements are used.
+     * 
+     * @param toModifyPosition    The position modified by the changers.
+     * @param toModifySpeed       The speed modified by the changers.
+     * @param toModifyAcceleration  The acceleration modified by the changers.
+     * @param toModifyRotation    The rotation modified by the changers.
+     * @return The modified movement characteristics (as a record).
+     */
     private MovCharacterizing applyModifiers(Pair<Double,Double> toModifyPosition, Pair<Double,Double> toModifySpeed,
                                 Pair<Double,Double> toModifyAcceleration, Pair<Double,Double> toModifyRotation) {
         Pair<Double,Double> modifiedPosition = toModifyPosition;
@@ -65,17 +168,17 @@ public final class Movement {
         Pair<Double,Double> modifiedAcceleration = toModifyAcceleration;
         Pair<Double,Double> modifiedRotation = toModifyRotation;
 
-        /* GRAVITY */
+        /* GRAVITY */ /* The entity will be constantly accelerated downwards*/
         if (this.listOfChangers.contains(MovementChangers.GRAVITY)) {
             modifiedSpeed = new Pair<>(modifiedSpeed.get1(), modifiedSpeed.get2() + GRAVITYMODIFIER);
         }
 
-        /* INVERSEGRAVITY */
+        /* INVERSEGRAVITY */ /* The entity will be constantly accelerated upwards*/
         if (this.listOfChangers.contains(MovementChangers.INVERSEGRAVITY)) {
             modifiedSpeed = new Pair<>(modifiedSpeed.get1(), modifiedSpeed.get2() + INVERSEGRAVITYMODIFIER);
         }
 
-        /* BOUNCING */
+        /* BOUNCING */ /* The entity has its speed and rotation inverted when touching one of the edges of the map*/
         if (this.listOfChangers.contains(MovementChangers.BOUNCING)) {
             if (this.movementSpecifiers.pos().get2() < MAPBOUNDUP) {
                 modifiedSpeed = new Pair<>(modifiedSpeed.get1(), Math.abs(modifiedSpeed.get2()));
@@ -87,7 +190,7 @@ public final class Movement {
             }
         }
 
-        /* BOUNDS */
+        /* BOUNDS */ /* The entity can't go further that the limits of the map*/
         if (this.listOfChangers.contains(MovementChangers.BOUNDS)) {
             if (this.movementSpecifiers.pos().get2() > MAPBOUNDDOWN) {
                 modifiedPosition = new Pair<>(modifiedPosition.get1(), MAPBOUNDDOWN);
@@ -105,6 +208,13 @@ public final class Movement {
         return new MovCharacterizing(modifiedPosition, modifiedSpeed, modifiedAcceleration, modifiedRotation);
     }
 
+    /**
+     * Updates the movement of the object by applying simple uniform or accelerated rectilinear motion formulas.
+     * Computes the new values of position, speed, acceleration and rotation and uses them as parameters for
+     * the methods of a builder. The method to calculate the relative position is also called here.
+     *
+     * @return The updated Movement as a new immutable class but with the new values computed.
+    */
     public Movement update() {
         final MovCharacterizing modifiedSpecifiers = this.applyModifiers(this.movementSpecifiers.pos(), this.movementSpecifiers.speed(), this.movementSpecifiers.acc(), this.movementSpecifiers.rot());
         Pair<Double,Double> modifiedPosition = modifiedSpecifiers.pos();
@@ -129,7 +239,29 @@ public final class Movement {
                             .build();
     }
 
-
+   /**
+     * Pattern builder: used here mainly because:
+     * 
+     * - all the parameters of the Movement class have a default value, which
+     * means that we would like to have all the possible combinations of
+     * constructors (one with three parameters, three with two parameters, three
+     * with a single parameter), which are way too many and confusing to use
+     * 
+     * - the Movement class has more parameters of the same type, and it is
+     * unclear to understand, in a call to its contructor, which is which. By using
+     * the builder, we emulate the so-called "named arguments". 
+     * 
+     * Since the Movement class is used by all entities and its values (position, speed, etc...)
+     * may need to be changed also from outside, this solution is better than accessing directly 
+     * the values or using lots of getters and setters, especially if only one getter for the Movement
+     * class is provided and the only way to change the values is by doing a get().set(newValue), which 
+     * "may expose internal representation by storing an externally mutable...". 
+     * The movement class was actually mutable before, but all these problems led to think that 
+     * an immutable, side-effects free implementation was better, even if more resource-consuming.
+     * (since a new class has to be created every time an entity has to update its movement, which actually
+     * happens a lot, but the number of entities which do this at all times is generally really low).
+     * 
+     */
     public static class Builder {
 
         private static final Pair<Double, Double> DEFAULT = new Pair<>(0.0,0.0);

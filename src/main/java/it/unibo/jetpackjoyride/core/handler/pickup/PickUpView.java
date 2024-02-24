8414c3db956bbcc7a1698f52e0b17cba4f5c3684
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2024-02-24 20:10:58 +0100

    ObstacleView and PickUpView now extend AbstractEntityView

--- src/main/java/it/unibo/jetpackjoyride/core/handler/pickup/PickUpView.java
+++ src/main/java/it/unibo/jetpackjoyride/core/handler/pickup/PickUpView.java
@@ -1,67 +1,73 @@
 package it.unibo.jetpackjoyride.core.handler.pickup;
 
 import java.util.List;
-
 import it.unibo.jetpackjoyride.core.entities.entity.api.Entity;
 import it.unibo.jetpackjoyride.core.entities.pickups.api.PickUp;
 import it.unibo.jetpackjoyride.core.entities.pickups.impl.VehiclePickUp;
-import it.unibo.jetpackjoyride.core.handler.entity.EntityView;
-import it.unibo.jetpackjoyride.utilities.GameInfo;
+import it.unibo.jetpackjoyride.core.handler.entity.AbstractEntityView;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
-public class PickUpView implements EntityView {
+public class PickUpView extends AbstractEntityView {
 
-    private final ImageView imageView;
-    private final List<Image> images;
-    private int animationFrame;
+    private final static Double VEHICLE_PICKUP_X_DIMENSION = 80.0;
+    private final static Double VEHICLE_PICKUP_Y_DIMENSION = 80.0;
+    private final static Integer VEHICLE_PICKUP_ANIMATION_LENGHT = 4;
+    private final static Integer VEHICLE_PICKUP_NUM_SPRITES = 8;
+
+    private final static Double VEHICLE_PICKUP_BANNER_X_DIMENSION = 1280.0;
+    private final static Double VEHICLE_PICKUP_BANNER_Y_DIMENSION = 240.0;
+    private final static Integer VEHICLE_PICKUP_BANNER_ANIMATION_LENGHT = 4;
+
+    private final static Integer LILSTOMPER_BANNER_SPRITES = 3;
+    private final static Integer MRCUDDLE_BANNER_SPRITES = 3;
+    private final static Integer PROFIT_BIRD_BANNER_SPRITES = 3;
+    private final static Integer DUKE_FISHRON_BANNER_SPRITES = 3;
+
+    private final static Double SHIELD_PICKUP_X_DIMENSION = 80.0;
+    private final static Double SHIELD_PICKUP_Y_DIMENSION = 80.0;
+    private final static Integer SHIELD_PICKUP_ANIMATION_LENGHT = 2;
+
     private int animationLenght;
     private int animationCounter;
 
     public PickUpView(final List<Image> images) {
-        this.images = images;
-        this.imageView = new ImageView();
+        super(images);
         this.animationFrame = 0;
         this.animationLenght = 1;
     }
 
     @Override
-    public void updateView(final Entity entity) {
+    protected void animateFrames(Entity entity) {
         final PickUp pickUp = (PickUp) entity;
-        Double width;
-        Double height;
-        final GameInfo infoResolution = GameInfo.getInstance();
-        final Double screenSizeX = infoResolution.getScreenWidth();
-        final Double screenSizeY = infoResolution.getScreenHeight();
 
         switch (pickUp.getPickUpType()) {
             case VEHICLE:
                 final VehiclePickUp vehiclePickUp = (VehiclePickUp) pickUp;
                 switch (pickUp.getEntityStatus()) {
                     case ACTIVE:
-                        width = screenSizeX / 15;
-                        height = screenSizeY / 9;
-                        animationLenght = 4;
-                        animationFrame = animationCounter / animationLenght % 8;
+                        width = VEHICLE_PICKUP_X_DIMENSION;
+                        height = VEHICLE_PICKUP_Y_DIMENSION;
+                        animationLenght = VEHICLE_PICKUP_ANIMATION_LENGHT;
+                        animationFrame = animationCounter / animationLenght % VEHICLE_PICKUP_NUM_SPRITES;
                         animationCounter++;
                         break;
                     case DEACTIVATED:
-                        width = screenSizeX;
-                        height = screenSizeY / 3;
-                        animationLenght = 4;
+                        width = VEHICLE_PICKUP_BANNER_X_DIMENSION;
+                        height = VEHICLE_PICKUP_BANNER_Y_DIMENSION;
+                        animationLenght = VEHICLE_PICKUP_BANNER_ANIMATION_LENGHT;
 
                         switch (vehiclePickUp.getVehicleSpawn()) {
                             case LILSTOMPER:
-                                animationFrame = 8 + animationCounter / animationLenght % 3;
+                                animationFrame = VEHICLE_PICKUP_NUM_SPRITES + animationCounter / animationLenght % LILSTOMPER_BANNER_SPRITES;
                                 break;
                             case MRCUDDLES:
-                                animationFrame = 11 + animationCounter / animationLenght % 3;
+                                animationFrame = VEHICLE_PICKUP_NUM_SPRITES + LILSTOMPER_BANNER_SPRITES + animationCounter / animationLenght % MRCUDDLE_BANNER_SPRITES;
                                 break;
                             case PROFITBIRD:
-                                animationFrame = 14 + animationCounter / animationLenght % 3;
+                                animationFrame = VEHICLE_PICKUP_NUM_SPRITES + LILSTOMPER_BANNER_SPRITES + MRCUDDLE_BANNER_SPRITES + animationCounter / animationLenght % PROFIT_BIRD_BANNER_SPRITES;
                                 break;
                             case DUKEFISHRON:
-                                animationFrame = 17 + animationCounter / animationLenght % 3;
+                                animationFrame = VEHICLE_PICKUP_NUM_SPRITES + LILSTOMPER_BANNER_SPRITES + MRCUDDLE_BANNER_SPRITES + PROFIT_BIRD_BANNER_SPRITES + animationCounter / animationLenght % DUKE_FISHRON_BANNER_SPRITES;
                                 break;
 
                             default:
@@ -80,9 +86,9 @@ public class PickUpView implements EntityView {
             case SHIELD:
                 switch (pickUp.getEntityStatus()) {
                     case ACTIVE:
-                        width = screenSizeX / 15;
-                        height = screenSizeY / 9;
-                        animationLenght = 2;
+                        width = SHIELD_PICKUP_X_DIMENSION;
+                        height = SHIELD_PICKUP_Y_DIMENSION;
+                        animationLenght = SHIELD_PICKUP_ANIMATION_LENGHT;
                         break;
                     case DEACTIVATED:
                         width = 0.0;
@@ -99,23 +105,5 @@ public class PickUpView implements EntityView {
                 break;
 
         }
-
-        final double scaleX = infoResolution.getScreenWidth() / infoResolution.getDefaultWidth();
-        final double scaleY = infoResolution.getScreenHeight() / infoResolution.getDefaultHeight();
-
-        imageView.setX(pickUp.getEntityMovement().getPosition().get1() * scaleX - width / 2);
-        imageView.setY(pickUp.getEntityMovement().getPosition().get2() * scaleY - height / 2);
-        imageView.setRotate(pickUp.getEntityMovement().getRotation().get1());
-
-        imageView.setFitWidth(width);
-        imageView.setFitHeight(height);
-
-        imageView.setImage(images.get(animationFrame));
     }
-
-    @Override
-    public ImageView getImageView() {
-        return imageView;
-    }
-
 }

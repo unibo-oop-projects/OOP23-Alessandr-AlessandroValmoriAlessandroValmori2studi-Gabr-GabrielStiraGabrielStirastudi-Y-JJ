6269428ba0df418d370894f1a3316393c875d1b6
Alessandro Valmori <alevalmorii@gmail.com>
2024-02-25 05:39:53 +0100

    added more

--- src/main/java/it/unibo/jetpackjoyride/utilities/CircularIterator.java
+++ src/main/java/it/unibo/jetpackjoyride/utilities/CircularIterator.java
@@ -3,20 +3,45 @@ package it.unibo.jetpackjoyride.utilities;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * @author alessandro.valmori2@studio.unibo.it
+ */
+
+/**
+ * A circular iterator for iterating through a list in a circular manner.
+ *
+ * @param <T> The type of elements in the list.
+ */
 public class CircularIterator<T> implements Iterator<T> {
     private List<T> list;
     private int currentIndex;
 
+    /**
+     * Constructs a CircularIterator with the specified list.
+     *
+     * @param list The list to iterate through.
+     */
     public CircularIterator(List<T> list) {
         this.list = list;
         this.currentIndex = 0;
     }
 
+    /**
+     * Checks if there are more elements in the iteration.
+     *
+     * @return true since the iteration is infinite.
+     */
     @Override
     public boolean hasNext() {
-        return true; // Always return true since the iteration is infinite
+        return true;
     }
 
+    /**
+     * Returns the next element in the iteration.
+     *
+     * @return The next element in the iteration.
+     * @throws IllegalStateException if the list is empty.
+     */
     @Override
     public T next() {
         if (list.isEmpty()) {
@@ -27,7 +52,4 @@ public class CircularIterator<T> implements Iterator<T> {
         currentIndex = (currentIndex + 1) % list.size(); // Wrap around to the beginning if end is reached
         return nextItem;
     }
-
-    
 }
-
